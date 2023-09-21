ca0ebaef470c14607c689e680ac02f04b045fd15
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-21 13:07:23 +0500

    completed

--- ex31/Program.cs
+++ ex31/Program.cs
@@ -10,7 +10,51 @@ namespace ex31
     {
         static void Main(string[] args)
         {
+            int arraySize = 10;
+            int[] array = new int[arraySize];
 
+            GenerateValues(array, 0, 101);
+
+            Shuffle(array);
+        }
+
+        static void GenerateValues(int[] array, int minValue, int maxValue)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minValue, maxValue);
+            }
+
+            WriteArray(array);
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+            int randomIndex;
+            int shuffledElement;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                randomIndex = random.Next(i, array.Length);
+                shuffledElement = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = shuffledElement;
+            }
+
+            WriteArray(array);
+        }
+
+        static void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
